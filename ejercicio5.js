ce8084e3233520dbc59cd12d593ac8c44a52ce2c
Stiven <usgarzon@ucundinamarca.edu.co>
2024-10-01 15:10:17 -0500

    Ejercicio5

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,34 +1,30 @@
 
 var reverse = function(x) {
-    let arreglo = [];
-    let arreglo2 =[];
-    let letras = x.toString();
-    let valor ;
+    //LIMITE DE 32 BIT
+    const limite = 2147483648; // 2^31
+    //NUMERO AL REVES
+    let valor = 0;
+    //SIGNO, OPERADOR TERNARIO
+    let signo = x < 0 ? -1 : 1;
+    //VALOR ABSOLUTO
+    x = Math.abs(x);
 
-    let indices;
-    for(let i = 0; i<letras.length;i++){
-        valor = letras.charAt(i);
-        arreglo.push(valor);    
+    while (x > 0) {
+        valor = valor * 10 + (x % 10);
+        console.log(valor)
+        x = Math.floor(x / 10);
     }
 
-    let iterador = 0;
-    if(arreglo[0]==='-'){
-        7 | 
+    valor *= signo;
+
+    // IMITES DE 32 BITS
+    if (valor < -limite || valor > limite - 1) {
+        return 0;
     }
 
-    for(let i = 0;i<arreglo.length;i++){
-        if(arreglo[0]==='-'){
-            arreglo2[0] = '-'
-            arreglo2.unshift(arreglo[i+1]);
-            console.log(arreglo2)
-        }
-          else{
-            arreglo2.unshift(arreglo[i]);
-            console.log(arreglo2)
-          } 
-       
-    }
- 
-   
-};
-console.log(reverse(-123))
+    return valor;
+}
+console.log(reverse(123))
+//console.log(2**31); POTENCIACION
+
+
